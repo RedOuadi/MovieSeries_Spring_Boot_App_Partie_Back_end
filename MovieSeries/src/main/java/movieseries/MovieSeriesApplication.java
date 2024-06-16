f03543ab04cfd621be047f35a5fd78cb3dd3ddd4
RedOuadi <redaouadi34@gmail.com>
2024-06-17 00:35:52 +0200

    changer MovieseriesApplication

--- MovieSeries/src/main/java/movieseries/MovieSeriesApplication.java
+++ MovieSeries/src/main/java/movieseries/MovieSeriesApplication.java
@@ -1,14 +1,19 @@
 package movieseries;
 
+
+
+
+
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "model")
 public class MovieSeriesApplication {
+
     public static void main(String[] args) {
+        SpringApplication.run(MovieSeriesApplication.class, args);
+    }
 
-
-    System.out.println("red");}}
+}
